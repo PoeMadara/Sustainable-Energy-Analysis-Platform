fac642880de622eaff33a266e18dc7f8df9586c1
Carlos Vergara <the.boss.patriot@gmail.com>
2024-10-17 21:23:36 +0200

    Big update
    Testing several models, functions, and configurations. Saving models and outputs. Selected a better new model, improving predictions and graphics.

--- functions.py
+++ functions.py
@@ -1,41 +1,41 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-def load_and_clean_data(filepath):
+def load_and_clean_data(filepath, scale=True):
     '''
     Carga y limpia el dataset, maneja los valores nulos, selecciona las columnas relevantes,
     escala los datos numéricos y devuelve el dataframe listo para usar.
-    
-    Parámetros:
-    filepath: Ruta del archivo CSV.
-    
-    Devuelve:
-    DataFrame limpio y listo para usar.
     '''
-    
     # Cargar el dataset
     df = pd.read_csv(filepath)
     
     # Seleccionar columnas clave (incluso year)
     columns_to_keep = [
-        'year', 'solar_consumption', 'wind_consumption', 'biofuel_consumption', 
-        'hydro_electricity', 'solar_electricity', 'wind_electricity', 'solar_cons_change_twh', 
-        'wind_cons_change_twh', 'biofuel_cons_change_pct', 'solar_share_elec', 'wind_share_elec',
-        'population', 'gdp'
+        'year', 'country', 'iso_code', 'population', 'gdp', 'electricity_demand',
+        'electricity_generation', 'electricity_share_energy',
+        'energy_cons_change_twh', 'energy_per_capita', 'energy_per_gdp', 
+        'solar_consumption', 'wind_consumption', 'biofuel_consumption', 'biofuel_electricity', 
+        'hydro_electricity', 'solar_electricity', 'wind_electricity', 
+        'solar_cons_change_twh', 'wind_cons_change_twh', 
+        'biofuel_cons_change_pct', 'solar_share_elec', 'wind_share_elec'
     ]
     
     # Filtrar solo las columnas útiles
     df = df[columns_to_keep]
     
-    # Manejo de valores nulos (imputación con la media en este caso)
-    df = df.fillna(df.mean())
+    # Manejo de valores nulos para columnas numéricas
+    numeric_cols = df.select_dtypes(include='number').columns
+    df[numeric_cols] = df[numeric_cols].fillna(df[numeric_cols].mean())
     
-    # Escalado de datos (excepto la columna 'year')
-    scaler = StandardScaler()
-    columns_to_scale = df.columns.difference(['year'])  # Escalamos todo menos 'year'
-    df[columns_to_scale] = scaler.fit_transform(df[columns_to_scale])
+    # Escalado de datos (excepto la columna 'year') si se requiere
+    if scale:
+        scaler = StandardScaler()
+        columns_to_scale = df.columns.difference(['year', 'country', 'iso_code'])
+        df[columns_to_scale] = scaler.fit_transform(df[columns_to_scale])
     
     return df
+
+
 
 def split_data(df):
     '''
@@ -57,3 +57,4 @@ def split_data(df):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
     return X_train, X_test, y_train, y_test
+
